0e47e1bb14b83e58149f4e5452709974b18533e7
fuxiaohei <fuxiaohei@vip.qq.com>
2023-09-20 22:23:26 +0800

    update ProjectCreate templates ui

--- src/components/project-create/TemplateTabs.jsx
+++ src/components/project-create/TemplateTabs.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button, Tab, Tabs, Dropdown } from "react-bootstrap";
+import { Button, Tab, Tabs, Row, Col, Nav, Dropdown } from "react-bootstrap";
 import SyntaxHighlighter from "react-syntax-highlighter";
 import { githubGist } from "react-syntax-highlighter/dist/esm/styles/hljs";
 import {
@@ -53,62 +53,91 @@ function TemplateTabs({ data, onCreate }) {
     onCreate(req);
   };
 
+  const renderTemplateListNav = (templates) => {
+    return templates.map((template) => {
+      return (
+        <Nav.Item key={template.name}>
+          <Nav.Link
+            onClick={() => {
+              setCurrentTemplate(template);
+            }}
+            eventKey={template.name}
+            className="px-2"
+          >
+            {template.template_name}
+          </Nav.Link>
+        </Nav.Item>
+      );
+    });
+  };
+
+  const renderTemplateListPane = (templates) => {
+    return templates.map((template) => {
+      return (
+        <Tab.Pane eventKey={template.name} key={template.name}>
+          <p className="text-secondary">{template.description}</p>
+          <SyntaxHighlighter
+            className="p-0"
+            wrapLongLines
+            language="rust"
+            style={githubGist}
+            showLineNumbers
+            lineNumberStyle={{ color: "#AAA", fontSize: "0.8rem" }}
+          >
+            {template.content}
+          </SyntaxHighlighter>
+        </Tab.Pane>
+      );
+    });
+  };
+
   return (
     <div>
       <Tabs
         defaultActiveKey={currentTemplate.language}
         transition={false}
         className="mb-3"
+        onSelect={(key) => {
+          setCurrentTemplate(data[key][0]);
+        }}
       >
         <Tab eventKey="rust" title="Rust">
-          <div className="border-bottom pb-3 mb-3">
-            <Dropdown as={"span"}>
-              <Dropdown.Toggle className="text-decoration-none" variant="link">
-                {currentTemplate.template_name}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {renderTemplateDropdown(data["rust"])}
-              </Dropdown.Menu>
-            </Dropdown>
-            <span className="text-secondary ms-3">
-              {currentTemplate.description}
-            </span>
-          </div>
-          <SyntaxHighlighter
-            className="p-0"
-            wrapLongLines
-            language="rust"
-            style={githubGist}
-            showLineNumbers
-            lineNumberStyle={{ color: "#AAA", fontSize: "0.8rem" }}
+          <Tab.Container
+            className="templates-list"
+            defaultActiveKey={data["rust"][0].name}
           >
-            {currentTemplate.content}
-          </SyntaxHighlighter>
+            <Row>
+              <Col sm={2}>
+                <Nav variant="pills" className="flex-column pb-3">
+                  {renderTemplateListNav(data["rust"])}
+                </Nav>
+              </Col>
+              <Col sm={10}>
+                <Tab.Content>
+                  {renderTemplateListPane(data["rust"])}
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
         </Tab>
         <Tab eventKey="javascript" title="JavaScript">
-          <div className="border-bottom pb-3 mb-3">
-            <Dropdown as={"span"}>
-              <Dropdown.Toggle className="text-decoration-none" variant="link">
-                {currentTemplate.template_name}
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {renderTemplateDropdown(data["javascript"])}
-              </Dropdown.Menu>
-            </Dropdown>
-            <span className="text-secondary ms-3">
-              {currentTemplate.description}
-            </span>
-          </div>
-          <SyntaxHighlighter
-            className="p-0"
-            wrapLongLines
-            language="rust"
-            style={githubGist}
-            showLineNumbers
-            lineNumberStyle={{ color: "#AAA", fontSize: "0.8rem" }}
+          <Tab.Container
+            className="templates-list"
+            defaultActiveKey={data["javascript"][0].name}
           >
-            {currentTemplate.content}
-          </SyntaxHighlighter>
+            <Row>
+              <Col sm={2}>
+                <Nav variant="pills" className="flex-column pb-3">
+                  {renderTemplateListNav(data["javascript"])}
+                </Nav>
+              </Col>
+              <Col sm={10}>
+                <Tab.Content>
+                  {renderTemplateListPane(data["javascript"])}
+                </Tab.Content>
+              </Col>
+            </Row>
+          </Tab.Container>
         </Tab>
       </Tabs>
       <div className="d-flex border-top pt-3 justify-content-end">
